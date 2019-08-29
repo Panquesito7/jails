f17a89e67012ad3faae2226b38b212c981dd3981
Panquesito7 <halfpacho@gmail.com>
2019-08-28 19:05:16 -0500

    Replace deprecated functions with newer ones

--- init.lua
+++ init.lua
@@ -1,26 +1,30 @@
 --[[
--- Jail mod by ShadowNinja
+Jails mod by ShadowNinja.
 --]]
 
 jails = {
 	jails = {},
 	filename = minetest.get_worldpath() .. "/jails.lua",
 	default = 0,
-	announce = minetest.setting_getbool("jails.announce"),
-	teleportDelay = tonumber(minetest.setting_get("jails.teleport_delay")) or 30,
+	announce = minetest.settings:get_bool("jails.announce"),
+	teleportDelay = tonumber(minetest.settings:get("jails.teleport_delay")) or 30,
 }
 
 local modPath = minetest.get_modpath(minetest.get_current_modname()) .. DIR_DELIM
 dofile(modPath .. "api.lua")
 dofile(modPath .. "commands.lua")
 
-minetest.register_privilege("jailer", "Can jail players")
+minetest.register_privilege("jailer", { 
+	description = "Can jail players",
+	give_to_singleplayer = false,
+	give_to_admin = true,
+})
 
 local function keepInJail(player)
 	local jailName, jail = jails:getJail(player:get_player_name())
 	if jail then
-		player:setpos(jail.pos)
-		return true  -- Don't spawn normaly
+		player:set_pos(jail.pos)
+		return true -- Don't spawn normaly
 	end
 end
 
@@ -40,4 +44,3 @@ minetest.register_globalstep(function(dtime)
 end)
 
 jails:load()
-
